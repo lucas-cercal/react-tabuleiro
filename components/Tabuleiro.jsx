187f4f004957e96790e4a451ae71d58e26f3fca5
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-07 17:18:02 -0300

    Projeto Tabuleiro #04

---
+++ components/Tabuleiro.jsx
@@ -0,0 +1,15 @@
+import Linha from "./Linha";
+
+export default function () {
+  return (
+    <div>
+      <Linha/>
+      <Linha preta/>
+      <Linha/>
+      <Linha preta/>
+      <Linha/>
+      <Linha preta/>
+      <Linha/>
+    </div>
+  )
+}
