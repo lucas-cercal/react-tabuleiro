2fc1207360f6a8c20a2742bd562edcf00540f5a3
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-07 17:00:09 -0300

    Projeto Tabuleiro #03

--- components/Subdivisao.jsx
+++ components/Subdivisao.jsx
@@ -1,7 +1,13 @@
 import styles from '../styles/Subdivisao.module.css'
 
 export default function Subdivisao(props) {
-  return <div className={styles.subdivisao}>
-    
-  </div>
+  return (
+    <div 
+    style={{
+      backgroundColor: props.preta ?  "#000" : "#fff"
+    }}
+    className={styles.subdivisao}>
+      
+    </div>
+  )
 }
